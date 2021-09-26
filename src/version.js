215922407809d3b8791cdd38aeac2f507dbf108d
Modestep444 <91279569+Modestep444@users.noreply.github.com>
2021-09-26 13:09:36 -0500

    Update version.js

--- src/version.js
+++ src/version.js
@@ -1,31 +1,29 @@
 const version = (prefix, pushname) => {
     return `
-*ѕнαηвσт ву ѕнαη∂υу*
+*Nelson Prince*
 
-*Actualizado:* 18 de Septiembre del 2021
-*Versión actual:* 4.0
-*Ofrecida por:* shanduy™
+*Actualizado:* 25 de Septiembre del 2021
+*Versión actual:* 4.1
+*Ofrecida por:* Nelson Prince
 
 
 *INFORME*
 
 Si no funciona el comando *play o *play2 checa el blog del bot y mira la version que poses 
-Ya que ire cambiando mi apikey de mi bot para que siga en funcionamiento
-Cambiare la apikey pasando un mes o si veo muchos atercados o robos
-Si vez a alguien que robo mi bot comunicate conmigo con el comando *creador
+comunicate conmigo con el comando *creador
 
 
-*COMO ACTUALIZAR EL BOT*
+*Mi Canal De YouTube*
 
-https://cutt.ly/dWp5uqE
+https://youtube.com/c/ModMaster%E3%83%84
 
 
 *SIGUEME EN INSTAGRAM 🥸🧉*
 
-https://www.instagram.com/thepavos
+https://www.instagram.com/nelsonprinceuwu/
 
 
-_*by shanduy*_
+_*by Nelson*_
 `
 
 }
